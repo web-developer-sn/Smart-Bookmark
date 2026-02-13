0c38b715ca0b8b9f42555dd0730c76cce8647eef
satendra <150445188+web-developer-sn@users.noreply.github.com>
2026-02-13 15:20:15 +0530

    Update page.tsx  domain issue
    /dashboard  =>  ${window.location.origin}/dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "/dashboard",
+        redirectTo: `${window.location.origin}/dashboard`,
       },
     });
   };
